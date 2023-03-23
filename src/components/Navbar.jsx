d9db43e10c11ec2a084dca6958364360c38db247
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 14:57:09 +0300

    Some changes were made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () =>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to='/'
-          className='flex items-center gap-2 '
+          className='flex items-center gap-2'
           onClick={() => 
           {
             setActive("")
