0ac9fe4ee0f617025f968477c75faa7912c9599a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 14:20:58 +0300

    Added the logo back

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -32,7 +32,7 @@ import tripguide from "./tripguide.png";
 
 export
 {
-  // logo,
+  logo,
   backend,
   creator,
   mobile,
