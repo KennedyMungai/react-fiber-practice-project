2d43c196af73d180c9cd0be64b6b698a4b2a65f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 13:49:10 +0300

    Created the active state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ import { navLinks } from '../constants'
 import { styles } from '../style'
 
 
-
 const Navbar = () =>
 {
+  const [active, setActive] = useState('')
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
