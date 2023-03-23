48e21eaddf8369a243456a08a137d08870abd3e0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 12:54:53 +0300

    Wrote the items to be rendered out

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import
   Route
 } from "react-router-dom";
 import
-  {
-    About,
-    Contact,
-    Experience,
-    Feedbacks,
-    Hero,
-    Loader,
-    Navbar,
-    Tech,
-    Works
-  } from "./components";
+{
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Loader,
+  Navbar,
+  Tech,
+  Works
+} from "./components";
 
 
 
 function App()
 {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no repeat bg-c">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </Router>
   );
 }
 
