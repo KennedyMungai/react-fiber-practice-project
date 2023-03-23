9addc52e9f7483cbb57a185e44e381ef99d77fc6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 13:45:17 +0300

    Added some classes to the nav tag inside the NavBar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () =>
 {
   return (
     <nav
-      className={`${styles.paddingX}`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
 
     </nav>
