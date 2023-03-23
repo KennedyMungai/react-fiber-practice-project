e731cb2293eeca776f765048bc8f86908278d4bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 12:48:05 +0300

    Imported and then exported all the components

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
 
-const index = () =>
+export
 {
-  return (
-    <div>index</div>
-  )
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
 }
-
-export default index
